1e5760f5b141dbe4b91cc3ce180262ff44716fb6
Danila Babanov <danila_babanov@yahoo.com>
2017-07-07 15:57:52 +0300

    array crud

---
+++ ruby/spec_array/lib/array.rb
@@ -0,0 +1,24 @@
+# Class Array
+class ArrayCRUD
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def create(*arg)
+    arg.map { |argument| @items << argument }
+  end
+
+  def delete(arg)
+    @items.delete(arg)
+  end
+
+  def show(arg)
+    @items.include?(arg) ? arg : nil
+  end
+
+  def update(arg, new_arg)
+    @items.include?(arg) ? @items[@items.index(arg)] = new_arg : nil
+  end
+end
---
+++ ruby/spec_array/spec/array_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+require_relative '../lib/array.rb'
+
+describe 'Class Array' do
+  let(:array) { ArrayCRUD.new }
+
+  context 'base class test' do
+    it 'defined? class' do
+      expect(array).to be_a(ArrayCRUD)
+    end
+
+    it 'responds array items' do
+      expect(array).to respond_to(:items)
+    end
+
+    it 'array items empty?' do
+      expect(array.items).to be_empty
+    end
+
+    it 'is a array' do
+      expect(array.items).to be_a(Array)
+    end
+  end
+end
+
+describe 'Methods class ArrayCRUD' do
+  let(:array) { ArrayCRUD.new }
+
+  context '#create' do
+  	it 'add items to array' do
+  	  array.create('book', 'phone', 'car')
+
+  	  expect(array.items).to eq(%w[book phone car])
+  	  expect(array.items.size).to eq(3)
+  	end
+  end
+
+  context '#delete' do
+    it 'delete element to array' do
+      array.create('book', 'phone', 'car')
+      array.delete('phone')
+
+      expect(array.items).not_to include('phone')
+  	  expect(array.items.size).to eq(2)
+    end
+  end
+
+  context '#show' do
+    it 'search and show element array' do
+      array.create('book', 'phone', 'car')
+
+      expect(array.show('phone')).to eq('phone')
+      expect(array.show('pc')).to be_nil
+    end
+  end
+
+  context '#update' do
+    it 'update element array' do
+      array.create('book', 'phone', 'car')
+      array.update('phone', 'new_element')
+
+      expect(array.items).to include('new_element')
+    end
+  end
+end
