488689c69faa84947138c3e2a372a1f42b5a12ce
Danila Babanov <danila_babanov@yahoo.com>
2017-07-11 10:49:51 +0300

    hash

--- ruby/spec_hash/lib/hash.rb
+++ ruby/spec_hash/lib/hash.rb
@@ -11,3 +11,22 @@ def update_name_hash(param)
   my_hash[:name] = param
   my_hash
 end
+
+def hash_each(hash)
+  hash.each { |k, v| puts "Hello, name: #{k}, age: #{v}." }
+end
+
+def all_posts
+  {
+    1 => { title: 'First post', 
+    	   tags: %w[first post], 
+    	   user_id: 1, 
+    	   comments: [
+    	   	          { body: 'First comment', user_id: 1 }, 
+    	   	          { body: 'Good post!', user_id: 4 }
+    	   	        ] 
+    	},
+    2 => { title: 'Second post', tags: %w[second post], user_id: 1 },
+    3 => { title: 'Ruby on Rails', tags: %w[ruby], user_id: 2 }
+  }
+end
--- ruby/spec_hash/spec/hash_spec.rb
+++ ruby/spec_hash/spec/hash_spec.rb
@@ -25,4 +25,61 @@ describe 'hash' do
       expect(update_name_hash('Alan')).to include(name: 'Alan', age: 26)
     end
   end
+
+  context '#hash_each' do
+    it 'each hash' do
+      hash = { 'Den' => 26, 'Leo' => 31 }
+      expect{ hash_each(hash) }.to output("Hello, name: Den, age: 26.\nHello, name: Leo, age: 31.\n").to_stdout
+    end
+  end
+
+  context 'all_post' do
+    it 'keys' do
+      expect(all_posts.keys).to eq([1,2,3])
+    end
+
+    it 'cout keys' do
+      expect(all_posts.keys.count).to eq(3)
+    end
+
+    it 'has_key?' do
+      expect(all_posts[1].has_key?(:title)).to be true
+    end
+
+    it '1 keys' do
+      expect(all_posts[1].keys).to eq(%i[title tags user_id comments])
+    end
+
+    it '1 values' do
+      expect(all_posts[1][:title]).to eq('First post')
+    end
+
+    it 'all 1 values' do
+       expect(all_posts[1].values).to eq(["First post",["first", "post"], 1, [{ :body=>"First comment", :user_id=>1 }, { :body=>"Good post!", :user_id=>4 }]])
+    end
+
+    it 'all 2 values' do
+       expect(all_posts[2].values).to eq(["Second post", ["second", "post"], 1])
+    end
+
+    it '1 values comments' do
+      expect(all_posts[1][:comments][0].values).to eq(["First comment", 1])
+    end
+
+    it 'tags' do
+      expect(all_posts[1][:tags]).to eq(%w[first post])
+    end
+
+    it 'include tags' do
+      expect(all_posts[1][:tags]).to include('first')
+    end
+
+    it 'not tags' do
+      expect(all_posts[1][:tags]).to_not include('ruby')
+    end
+
+    it 'match array' do
+      expect(all_posts[1][:comments]).to match_array([{:body=>"First comment", :user_id=>1}, {:body=>"Good post!", :user_id=>4}])
+    end
+  end
 end
