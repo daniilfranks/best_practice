983ac57c302f3ff67c24615ab91718de0e8f8824
Danila Babanov <danila_babanov@yahoo.com>
2017-07-12 20:34:01 +0300

    test scraper

--- ruby/scraper_tv_program/spec/scraper_spec.rb
+++ ruby/scraper_tv_program/spec/scraper_spec.rb
@@ -11,4 +11,26 @@ describe 'class Scraper' do
       expect(doc).to be_a(Nokogiri::HTML::Document)
     end
   end
+
+  describe '#get_program' do
+    it 'uses a CSS selector to return an array of Nokogiri XML elements representing the courses described on the web page we are scraping' do
+      doc = scraper.get_program
+
+      expect(doc).to be_a(Nokogiri::XML::NodeSet)
+      expect{ doc.css(".tv-channel-title__text").text }.to_not raise_error
+    end
+  end
+
+  describe '#make_programs' do
+    it 'class variable test' do
+      Program.reset_all
+      doc = scraper.make_programs
+
+      Program.all.each do |program|
+      	expect(program.id).to be_a(Integer)
+        expect(program.title).to be_a(String)
+        expect(program.tv_programs).to be_a(Array)
+      end
+    end
+  end
 end
