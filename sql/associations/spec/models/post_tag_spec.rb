594b6d59a49524332871bd956401bc10ff2deb12
Danila Babanov <danila_babanov@yahoo.com>
2017-07-20 18:52:52 +0300

    rspec

--- sql/associations/spec/models/post_tag_spec.rb
+++ sql/associations/spec/models/post_tag_spec.rb
@@ -8,4 +8,28 @@ describe 'class post_tag' do
   it 'has a name' do
     expect(PostTag.where(name: 'languages').first).to eq(@post_tag)
   end
+
+  it 'has a post_tag' do
+    tag = Tag.create(name: 'ruby')
+    @post_tag.tag = tag
+    @post_tag.save
+
+    expect(PostTag.find_by(name: 'languages').tag).to eq(tag)
+  end
+
+  it 'has a post' do
+    post = Post.create(name: 'First post')
+    @post_tag.post = post
+    @post_tag.save
+
+    expect(PostTag.where(name: 'languages').first.post).to eq(post)
+  end
+
+  it 'can build tag' do
+    tag = @post_tag.build_tag(name: 'ruby')
+    @post_tag.save
+
+    expect(PostTag.where(name: 'languages').first.tag).to eq(tag)
+    expect(Tag.where(name: 'ruby').first.post_tags).to include(@post_tag)
+  end
 end
--- sql/associations/spec/models/tag_spec.rb
+++ sql/associations/spec/models/tag_spec.rb
@@ -8,4 +8,22 @@ describe 'class Tag' do
   it 'has a name' do
     expect(Tag.where(name: 'ruby').first).to eq(@tag)
   end
+
+  it 'has many post_tags' do
+    @tag.post_tags << PostTag.create(name: 'languages')
+    @tag.save
+
+    found_post_tag = PostTag.find_by(name: 'languages')
+
+    expect(found_post_tag.tag).to eq(@tag)
+  end
+
+  it 'it also association with an post' do
+    post = Post.create(name: 'First post')
+    post_tag = PostTag.create(name: 'languages', tag: @tag)
+    post.post_tags << post_tag
+    post.save
+
+    expect(@tag.posts).to include(post)
+  end
 end
