9f136b1661d65255a36b5de7a06627f2ed63fed4
Danila Babanov <danila_babanov@yahoo.com>
2017-07-27 15:52:08 +0300

    spec item

--- sinatra/sinatra_forms/spec/models/item_spec.rb
+++ sinatra/sinatra_forms/spec/models/item_spec.rb
@@ -19,3 +19,31 @@ describe 'class Item' do
     expect(book.price).to eq(120)
   end
 end
+
+describe 'get/post' do
+  it 'request status page' do
+    get '/items/new'
+
+    expect(last_response.status).to eq(200)
+  end
+
+  it 'test form' do
+    visit '/items/new'
+
+    expect(page).to have_selector('form')
+    expect(page).to have_field('item[name]')
+  end
+
+  it 'display resault' do
+    visit '/items/new'
+
+    fill_in('item[name]', with: 'Den')
+    fill_in('item[description]', with: 'text text')
+    fill_in('item[price]', with: 120)
+    click_button('Create item')
+
+    expect(page).to have_text('Den')
+    expect(page).to have_text('text text')
+    expect(page).to have_text('120')
+  end
+end
