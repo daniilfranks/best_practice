04f74d03c5f3cbdfa797a9540f694a38f58019bc
Danila Babanov <danila_babanov@yahoo.com>
2017-07-28 11:34:25 +0300

    session

--- sinatra/sessions/app/controller/application_controller.rb
+++ sinatra/sessions/app/controller/application_controller.rb
@@ -5,7 +5,18 @@ class ApplicationController < Sinatra::Base
 
     use Rack::Session::Cookie, :key => 'rack.session',
                                :expire_after => 2592000,
-                               :secret => SecureRandom.hex(64)
+                               :secret => 'a1v1F0O8xE7zA293lJ6cN1'
+  end
+
+  USER = { user_id: 1, email: 'babanovs5@gmail.com', password: 12345 }
+
+  # redirect
+  def curent_user
+    redirect '/' unless session[:user_id].nil?
+  end
+
+  def authorized
+    redirect '/sign_in' if session[:user_id].nil?
   end
 
   get '/' do
@@ -22,6 +33,48 @@ class ApplicationController < Sinatra::Base
 
   post '/tests' do
     session[:login] = params['test']['login']
-    redirect to '/tests'
+    redirect '/tests'
+  end
+
+  # Sign in
+  get '/sign_in' do
+    curent_user
+    erb :'users/sign_in'
+  end
+
+  post '/sign_in' do
+    user_id = params['user']['user_id']
+    email = params['user']['email']
+    password = params['user']['password']
+
+    if USER[:user_id] == user_id && USER[:email] == email && USER[:password] == password
+      redirect '/sign_in'
+    else
+      session[:user_id] = params['user']['user_id']
+      redirect '/'
+    end
+  end
+
+  # Sign up
+  get '/sign_up' do
+    curent_user
+    erb :'users/sign_up'
+  end
+
+  post '/sign_up' do
+    session[:user_id] = params['user']['user_id']
+
+    redirect '/'
+  end
+
+  get '/logout' do
+    session.clear
+
+    redirect '/'
+  end
+
+  get '/private' do
+    authorized
+    erb :'/private'
   end
 end
