1395407865c84d229b452e6ff05ed9b5dda3ca62
Danila Babanov <danila_babanov@yahoo.com>
2017-08-21 10:53:08 +0300

    base methods cart

--- sinatra/store2/app/models/book_cart.rb
+++ sinatra/store2/app/models/book_cart.rb
@@ -1,3 +1,12 @@
 class BookCart
+  attr_accessor :book_id, :quantity
 
+  def initialize(book_id, quantity)
+    @book_id = book_id
+    @quantity = quantity
+  end
+
+  def books
+    Book.find(@book_id)
+  end
 end
--- sinatra/store2/app/models/cart.rb
+++ sinatra/store2/app/models/cart.rb
@@ -1,3 +1,25 @@
 class Cart
+  attr_accessor :books
 
+  def initialize(books = [])
+    @books = books
+  end
+
+  def add_book(book_id, quantity = 1)
+  	resault = @books.find { |book| book.book_id == book_id }
+
+  	if resault
+      resault.quantity += quantity
+  	else
+      @books << BookCart.new(book_id, quantity)
+    end
+  end
+
+  def delete_book(book_id)
+    @books.delete_if { |book| book.book_id == book_id }
+  end
+
+  def clear_cart
+    @books.clear
+  end
 end
