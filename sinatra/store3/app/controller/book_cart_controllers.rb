2cb477b12cd2f2dfae2541e2e0c70b2ce525fcfd
Danila Babanov <danila_babanov@yahoo.com>
2017-08-23 12:43:19 +0300

    cart

--- sinatra/store3/app/controller/book_cart_controllers.rb
+++ sinatra/store3/app/controller/book_cart_controllers.rb
@@ -10,7 +10,7 @@ class BookCartsController < ApplicationController
   post '/cart' do
   	book = Book.find(params['cart']['book_id'])
   	#book_carts = @cart.book_carts.build(book: book)
-  	book_carts = @cart.add_book(book.id)
+  	book_carts = @cart.add_book(book.id, params['cart']['quantity'])
 
   	if book_carts.save
   	  flash[:notice] = 'Successfully add to cart!'
--- sinatra/store3/app/models/cart.rb
+++ sinatra/store3/app/models/cart.rb
@@ -1,13 +1,13 @@
 class Cart < ActiveRecord::Base
   has_many :book_carts, dependent: :destroy
 
-  def add_book(book_id)
+  def add_book(book_id, quantity = 1)
     book = book_carts.find_by(book_id: book_id)
 
     if book
       book.quantity += 1
     else
-      book = book_carts.build(book_id: book_id)
+      book = book_carts.build(book_id: book_id, quantity: quantity)
     end
     book
   end
