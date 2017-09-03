6632e4b92c99962ba00152222452cc2d5eb1a4cb
Danila Babanov <danila_babanov@yahoo.com>
2017-09-03 16:10:14 +0300

    send grid

---
+++ ruby/mail/api.rb
@@ -0,0 +1,16 @@
+# gem install sendgrid-ruby
+
+require 'sendgrid-ruby'
+include SendGrid
+
+from = Email.new(email: 'admin@danilababanov.ovh')
+to = Email.new(email: 'babanovs5@gmail.com')
+subject = 'Hi boban'
+content = Content.new(type: 'text/plain', value: 'My text')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
